ab1bab10b984e3f025c436b98550183e51c378d8
dhivyagj10-bit <dhivyagj10@gmail.com>
2026-03-23 23:20:57 +0530

    Add files via upload

---
+++ oddoreven.js
@@ -0,0 +1,11 @@
+function checkEvenorOdd() {
+   let number = 9;
+
+         if ( number % 2 === 0) {
+                console.log(number + " is even.");
+        } else {
+                console.log(number + " is odd.");
+        }
+}       
+
+checkEvenorOdd();

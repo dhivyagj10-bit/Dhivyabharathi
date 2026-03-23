fa5539c6e8c190b6574533e1485ee39e4a18cde5
dhivyagj10-bit <dhivyagj10@gmail.com>
2026-03-23 23:35:12 +0530

    Add files via upload

---
+++ numtypeidentify.js
@@ -0,0 +1,14 @@
+function numtypeidentify() {
+    let num = -1;
+    if (num > 0) {
+       console.log("positive");
+        
+    }   else if (num < 0) { 
+        console.log("negative");
+        
+
+    }   else {
+        console.log("Neutral");
+    }
+}
+numtypeidentify();

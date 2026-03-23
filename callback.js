da479d4301e17b7c3681f9fbc59fe9e1fa35478f
dhivyagj10-bit <dhivyagj10@gmail.com>
2026-03-23 18:13:11 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,6 @@
+function checkBrowserVersion() {
+    let browser = "145.0.7632.75/7.0 (Official Build) (64-bit)";
+    console.log("Google Chrome browser version is " + browser);
+
+}
+setTimeout(checkBrowserVersion, 1000);

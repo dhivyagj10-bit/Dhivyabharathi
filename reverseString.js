d1fc6a8f0a678907ad82f81a1f2ff8cffec4aac0
dhivyagj10-bit <dhivyagj10@gmail.com>
2026-03-24 18:27:25 +0530

    Add files via upload

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+let reverse = 'dhivya';
+ let reversed = reverse.split('').reverse().join('');
+console.log(reversed);
+if (reverse === reversed) {
+    console.log('True');
+  console.log('The reversed string is palindrome: ' + reversed);
+} else {
+    console.log('False');
+  console.log('The reversed string is not palindrome: ' + reversed);
+};
+   
